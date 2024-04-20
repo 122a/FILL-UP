e27a7ac479097c669d5a089fdda3cb93cb8e45f3
chan <chan0822kr@gmail.com>
2024-04-21 08:30:29 +0900

    commit 3
    내용:
    •SRC / front / lib / main.dart(수정)
     - 로그인 화면 UI 구현
     - 각종 함수 및 기능 아직 미완성
     - 근데 로그인 Page의 경우, 추후 많이 바뀔 가능성이 있음.
    
    추가할 것:
    • 백엔드로 로그인 또는 간편 로그인 구현 추가할 것
    • 비밀번호 찾기 기능, 회원가입 기능이 지금 사실 없고 텍스트로만 표시한거라 시간 남으면 회원가입 기능 구현 해주길.
    • 특히 간편로그인 기능 추가 해주길(특히중요.)

--- 소스코드(SRC)/front/lib/main.dart
+++ 소스코드(SRC)/front/lib/main.dart
@@ -1,125 +1,211 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatefulWidget {
+  const App({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      // 앱에서 공통적으로 쓰이는 속성, 컬러들
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        // cursor 색상
+        textSelectionTheme: const TextSelectionThemeData(
+          cursorColor: Color(0xff004fff),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      home: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            //양 옆 공백 20px
+            horizontal: 20,
+          ),
+          child: Column(
+            children: [
+              const SizedBox(
+                //천장과 로고 사이의 여백
+                height: 175,
+              ),
+              Row(
+                children: [
+                  const SizedBox(
+                    //로고와 왼쪽 벽사이의 여백
+                    width: 96,
+                  ),
+                  Container(
+                    //로고
+                    child: Image.asset('assets/fill_up_logo.png'),
+                  ),
+                  const Text(
+                    "Fill UP",
+                    style: TextStyle(
+                        fontSize: 28,
+                        fontFamily: 'SF-Pro-Display',
+                        fontWeight: FontWeight.w700),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                //로고와 입력창 사이의 여백
+                height: 150,
+              ),
+              Form(
+                child: Column(
+                  children: [
+                    TextFormField(
+                      //아이디 입력창
+                      decoration: const InputDecoration(
+                        enabledBorder: UnderlineInputBorder(
+                          // default 입력창
+                          borderSide: BorderSide(
+                            color: Color(0xffC4C3C3),
+                            width: 1.0,
+                          ),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          //클릭했을 때 입력창
+                          borderSide: BorderSide(
+                            color: Color(0xff004fff),
+                          ),
+                        ),
+                        hintText: " 이메일을 입력해주세요.",
+                        labelStyle: TextStyle(
+                          fontFamily: "SF-Pro-Rounded",
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      //아이디 비밀번호 입력창 사이의 여백
+                      height: 10,
+                    ),
+                    TextFormField(
+                      // 비밀번호 입력창
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                        suffixIcon: Icon(Icons.lock_outline), // 비밀번호 아이콘
+                        enabledBorder: UnderlineInputBorder(
+                          // default 입력창
+                          borderSide: BorderSide(
+                            color: Color(0xffC4C3C3),
+                            width: 1.0,
+                          ),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          //클릭했을 때 입력창
+                          borderSide: BorderSide(
+                            color: Color(0xff004fff),
+                          ),
+                        ),
+                        hintText: " 비밀번호를 입력해주세요.",
+                        labelStyle: TextStyle(
+                          fontFamily: "SF-Pro-Rounded",
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      // 입력창과 "비밀번호 잊으셨습니까" 안내 문구 사이의 여백
+                      height: 16,
+                    ),
+                    const Row(
+                      //"비밀번호 잊으셨나요?" 안내문구
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          "비밀번호를 잊으셨나요? ",
+                          style: TextStyle(
+                            fontFamily: "SF-PRO-Rounded",
+                            fontSize: 14,
+                            fontWeight: FontWeight.w700,
+                            color: Color(0xff606060),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      // "비밀번호 잊으셨나요?" 안내문구와 로그인 버튼 사이의 여백
+                      height: 16,
+                    ),
+                    ElevatedButton(
+                      // 로그인 버튼
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xff0b44f4),
+                        minimumSize: const Size(353, 48),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                      child: const Text(
+                        "로그인",
+                        style: TextStyle(
+                          color: Color(0xFFffffff),
+                          fontFamily: "SF-Pro-Rounded",
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                // 로그인 버튼과 간편로그인 info 사이의 간격
+                height: 12,
+              ),
+              Container(
+                // 간편로그인 안내 information
+                child: Image.asset('assets/simple_login_info.png'),
+              ),
+              const SizedBox(
+                //로그인 info와 회원가입 버튼 사이의 간격 (이 건 추 후 수정 될 가능성 큼)
+                height: 144,
+              ),
+              const Row(
+                // 회원가입 안내와 버튼
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "아직 계정이 없나요?",
+                    style: TextStyle(
+                      fontFamily: "SF-Pro-Rounded",
+                      fontSize: 13,
+                      fontWeight: FontWeight.w600,
+                      color: Color(
+                        0xff747474,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    "  회원가입",
+                    style: TextStyle(
+                      fontFamily: "SF-Pro-Rounded",
+                      fontSize: 13,
+                      fontWeight: FontWeight.w600,
+                      color: Color(
+                        0xff0b44f4,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
