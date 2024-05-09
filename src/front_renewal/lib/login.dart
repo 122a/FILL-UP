bd2553df72c83767f002213a46cb046ad2c2f93d
chan <chan0822kr@gmail.com>
2024-05-09 10:01:43 +0900

    commit 13
    내용 :
    • src > lib
     - 모든 위젯들 Stateless로 바꿈.
     - GetX 패키지 적용
     - 기존의 Go Router 패키지는 삭제

--- src/front_renewal/lib/login.dart
+++ src/front_renewal/lib/login.dart
@@ -4,279 +4,269 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:front_renewal/main.dart';
-import 'package:go_router/go_router.dart';
+import 'package:front_renewal/signup.dart';
+import 'package:get/get.dart';
 
-class Login extends StatefulWidget {
+class Login extends StatelessWidget {
   const Login({super.key});
 
   @override
-  State<Login> createState() => _LoginState();
-}
-
-class _LoginState extends State<Login> {
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // 앱에서 공통적으로 쓰이는 속성, 컬러들
-      theme: ThemeData(
-        // cursor 색상
-        textSelectionTheme: const TextSelectionThemeData(
-          cursorColor: Color(0xff004fff),
-          selectionColor: Color.fromARGB(135, 0, 81, 255),
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          //양 옆 공백 20px
+          horizontal: 20,
         ),
-      ),
-      home: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.symmetric(
-            //양 옆 공백 20px
-            horizontal: 20,
-          ),
-          child: Column(
-            children: [
-              const SizedBox(
-                //천장과 로고 사이의 여백
-                height: 175,
-              ),
-              Row(
-                children: [
-                  const SizedBox(
-                    //로고와 왼쪽 벽사이의 여백
-                    width: 80,
+        child: Column(
+          children: [
+            const SizedBox(
+              //천장과 로고 사이의 여백
+              height: 175,
+            ),
+            Row(
+              children: [
+                const SizedBox(
+                  //로고와 왼쪽 벽사이의 여백
+                  width: 80,
+                ),
+                Container(
+                  //로고
+                  child: Image.asset(
+                    'assets/fill_up_logo.png',
+                    width: 53,
+                    height: 51,
+                    fit: BoxFit.fill,
                   ),
-                  Container(
-                    //로고
-                    child: Image.asset(
-                      'assets/fill_up_logo.png',
-                      width: 53,
-                      height: 51,
-                      fit: BoxFit.fill,
+                ),
+                const Text(
+                  "Fill UP",
+                  style: TextStyle(
+                      fontSize: 28,
+                      fontFamily: 'SF-Pro-Display',
+                      fontWeight: FontWeight.w700),
+                ),
+              ],
+            ),
+            const SizedBox(
+              //로고와 입력창 사이의 여백
+              height: 100,
+            ),
+            Form(
+              child: Column(
+                children: [
+                  TextFormField(
+                    //아이디 입력창
+                    decoration: const InputDecoration(
+                      enabledBorder: UnderlineInputBorder(
+                        // default 입력창
+                        borderSide: BorderSide(
+                          color: Color(0xffC4C3C3),
+                          width: 1.0,
+                        ),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        //클릭했을 때 입력창
+                        borderSide: BorderSide(
+                          color: Color(0xff004fff),
+                        ),
+                      ),
+                      hintText: " 이메일을 입력해주세요.",
+                      labelStyle: TextStyle(
+                        fontFamily: "SF-Pro-Rounded",
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                      ),
                     ),
                   ),
-                  const Text(
-                    "Fill UP",
-                    style: TextStyle(
-                        fontSize: 28,
-                        fontFamily: 'SF-Pro-Display',
-                        fontWeight: FontWeight.w700),
+                  const SizedBox(
+                    //아이디 비밀번호 입력창 사이의 여백
+                    height: 10,
+                  ),
+                  TextFormField(
+                    // 비밀번호 입력창
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      suffixIcon: Icon(Icons.lock_outline), // 비밀번호 아이콘
+                      enabledBorder: UnderlineInputBorder(
+                        // default 입력창
+                        borderSide: BorderSide(
+                          color: Color(0xffC4C3C3),
+                          width: 1.0,
+                        ),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        //클릭했을 때 입력창
+                        borderSide: BorderSide(
+                          color: Color(0xff004fff),
+                        ),
+                      ),
+                      hintText: " 비밀번호를 입력해주세요.",
+                      labelStyle: TextStyle(
+                        fontFamily: "SF-Pro-Rounded",
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    // 입력창과 "비밀번호 잊으셨습니까" 안내 문구 사이의 여백
+                    height: 16,
+                  ),
+                  const Row(
+                    //"비밀번호 잊으셨나요?" 안내문구
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        "비밀번호를 잊으셨나요? ",
+                        style: TextStyle(
+                          fontFamily: "SF-PRO-Rounded",
+                          fontSize: 14,
+                          fontWeight: FontWeight.w700,
+                          color: Color(0xff606060),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    // "비밀번호 잊으셨나요?" 안내문구와 로그인 버튼 사이의 여백
+                    height: 16,
+                  ),
+                  ElevatedButton(
+                    // 로그인 버튼
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xff0b44f4),
+                      minimumSize: const Size(353, 48),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                    child: const Text(
+                      "로그인",
+                      style: TextStyle(
+                        color: Color(0xFFffffff),
+                        fontFamily: "SF-Pro-Rounded",
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
                   ),
                 ],
               ),
-              const SizedBox(
-                //로고와 입력창 사이의 여백
-                height: 100,
-              ),
-              Form(
-                child: Column(
-                  children: [
-                    TextFormField(
-                      //아이디 입력창
-                      decoration: const InputDecoration(
-                        enabledBorder: UnderlineInputBorder(
-                          // default 입력창
-                          borderSide: BorderSide(
-                            color: Color(0xffC4C3C3),
-                            width: 1.0,
-                          ),
-                        ),
-                        focusedBorder: UnderlineInputBorder(
-                          //클릭했을 때 입력창
-                          borderSide: BorderSide(
-                            color: Color(0xff004fff),
-                          ),
-                        ),
-                        hintText: " 이메일을 입력해주세요.",
-                        labelStyle: TextStyle(
-                          fontFamily: "SF-Pro-Rounded",
-                          fontSize: 16,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      //아이디 비밀번호 입력창 사이의 여백
-                      height: 10,
-                    ),
-                    TextFormField(
-                      // 비밀번호 입력창
-                      obscureText: true,
-                      decoration: const InputDecoration(
-                        suffixIcon: Icon(Icons.lock_outline), // 비밀번호 아이콘
-                        enabledBorder: UnderlineInputBorder(
-                          // default 입력창
-                          borderSide: BorderSide(
-                            color: Color(0xffC4C3C3),
-                            width: 1.0,
-                          ),
-                        ),
-                        focusedBorder: UnderlineInputBorder(
-                          //클릭했을 때 입력창
-                          borderSide: BorderSide(
-                            color: Color(0xff004fff),
-                          ),
-                        ),
-                        hintText: " 비밀번호를 입력해주세요.",
-                        labelStyle: TextStyle(
-                          fontFamily: "SF-Pro-Rounded",
-                          fontSize: 16,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      // 입력창과 "비밀번호 잊으셨습니까" 안내 문구 사이의 여백
-                      height: 16,
-                    ),
-                    const Row(
-                      //"비밀번호 잊으셨나요?" 안내문구
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Text(
-                          "비밀번호를 잊으셨나요? ",
-                          style: TextStyle(
-                            fontFamily: "SF-PRO-Rounded",
-                            fontSize: 14,
-                            fontWeight: FontWeight.w700,
-                            color: Color(0xff606060),
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(
-                      // "비밀번호 잊으셨나요?" 안내문구와 로그인 버튼 사이의 여백
-                      height: 16,
-                    ),
-                    ElevatedButton(
-                      // 로그인 버튼
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xff0b44f4),
-                        minimumSize: const Size(353, 48),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                      ),
-                      child: const Text(
-                        "로그인",
-                        style: TextStyle(
-                          color: Color(0xFFffffff),
-                          fontFamily: "SF-Pro-Rounded",
-                          fontSize: 16,
-                          fontWeight: FontWeight.w700,
-                        ),
-                      ),
-                    ),
-                  ],
+            ),
+            const SizedBox(
+              // 로그인 버튼과 간편로그인 info 사이의 간격
+              height: 12,
+            ),
+            Container(
+              // 간편로그인 안내 information
+              child: Image.asset('assets/simple_login_info.png'),
+            ),
+            SizedBox(
+              //네이버 로그인 버튼
+              width: 353,
+              height: 48,
+              child: OutlinedButton.icon(
+                onPressed: () {},
+                style: OutlinedButton.styleFrom(
+                  side: const BorderSide(
+                    color: Color.fromRGBO(107, 107, 109, 0.3),
+                    width: 1,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+                icon: SvgPicture.asset(
+                  'assets/icons/naver_icon.svg',
+                  width: 35,
+                  height: 35,
+                ),
+                label: const Text(
+                  "네이버 로그인",
+                  style: TextStyle(
+                    color: Color(0xff6b6b6d),
+                    fontFamily: "SF-Pro-Display",
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
               ),
-              const SizedBox(
-                // 로그인 버튼과 간편로그인 info 사이의 간격
-                height: 12,
-              ),
-              Container(
-                // 간편로그인 안내 information
-                child: Image.asset('assets/simple_login_info.png'),
-              ),
-              SizedBox(
-                //네이버 로그인 버튼
-                width: 353,
-                height: 48,
-                child: OutlinedButton.icon(
-                  onPressed: () {},
-                  style: OutlinedButton.styleFrom(
-                    side: const BorderSide(
-                      color: Color.fromRGBO(107, 107, 109, 0.3),
-                      width: 1,
-                    ),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
-                    ),
+            ),
+            const SizedBox(
+              // 네이버 로그인 버튼과 구글 로그인 버튼 사이 여백
+              height: 16,
+            ),
+            SizedBox(
+              //구글 로그인 버튼
+              width: 353,
+              height: 48,
+              child: OutlinedButton.icon(
+                onPressed: () {},
+                style: OutlinedButton.styleFrom(
+                  side: const BorderSide(
+                    color: Color.fromRGBO(107, 107, 109, 0.3),
+                    width: 1,
                   ),
-                  icon: SvgPicture.asset(
-                    'assets/icons/naver_icon.svg',
-                    width: 35,
-                    height: 35,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
                   ),
-                  label: const Text(
-                    "네이버 로그인",
+                ),
+                icon: SvgPicture.asset(
+                  'assets/icons/google_icon.svg',
+                  width: 35,
+                  height: 35,
+                ),
+                label: const Text(
+                  "구글  로그인",
+                  style: TextStyle(
+                    color: Color(0xff6b6b6d),
+                    fontFamily: "SF-Pro-Display",
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextButton(
+              // 회원가입 페이지로 향하는 텍스트버튼
+              onPressed: () {
+                Get.to(
+                  const SignUp(),
+                );
+              },
+              child: const Row(
+                // 회원가입 안내와 버튼
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "아직 계정이 없나요?",
                     style: TextStyle(
-                      color: Color(0xff6b6b6d),
-                      fontFamily: "SF-Pro-Display",
-                      fontSize: 16,
+                      fontFamily: "SF-Pro-Rounded",
+                      fontSize: 13,
                       fontWeight: FontWeight.w600,
+                      color: Color(
+                        0xff747474,
+                      ),
                     ),
                   ),
-                ),
-              ),
-              const SizedBox(
-                // 네이버 로그인 버튼과 구글 로그인 버튼 사이 여백
-                height: 16,
-              ),
-              SizedBox(
-                //구글 로그인 버튼
-                width: 353,
-                height: 48,
-                child: OutlinedButton.icon(
-                  onPressed: () {},
-                  style: OutlinedButton.styleFrom(
-                    side: const BorderSide(
-                      color: Color.fromRGBO(107, 107, 109, 0.3),
-                      width: 1,
-                    ),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                  ),
-                  icon: SvgPicture.asset(
-                    'assets/icons/google_icon.svg',
-                    width: 35,
-                    height: 35,
-                  ),
-                  label: const Text(
-                    "구글  로그인",
+                  Text(
+                    "  회원가입",
                     style: TextStyle(
-                      color: Color(0xff6b6b6d),
-                      fontFamily: "SF-Pro-Display",
-                      fontSize: 16,
+                      fontFamily: "SF-Pro-Rounded",
+                      fontSize: 13,
                       fontWeight: FontWeight.w600,
+                      color: Color(0xff0b44f4),
                     ),
                   ),
-                ),
+                ],
               ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextButton(
-                // 회원가입 페이지로 향하는 텍스트버튼
-                onPressed: () => context.go('/details'),
-                child: const Row(
-                  // 회원가입 안내와 버튼
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "아직 계정이 없나요?",
-                      style: TextStyle(
-                        fontFamily: "SF-Pro-Rounded",
-                        fontSize: 13,
-                        fontWeight: FontWeight.w600,
-                        color: Color(
-                          0xff747474,
-                        ),
-                      ),
-                    ),
-                    Text(
-                      "  회원가입",
-                      style: TextStyle(
-                        fontFamily: "SF-Pro-Rounded",
-                        fontSize: 13,
-                        fontWeight: FontWeight.w600,
-                        color: Color(0xff0b44f4),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
--- src/front_renewal/lib/main.dart
+++ src/front_renewal/lib/main.dart
@@ -4,43 +4,19 @@ import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:front_renewal/login.dart';
 import 'package:front_renewal/signup.dart';
-import 'package:go_router/go_router.dart';
+import 'package:get/get.dart';
 
-void main(){
+void main() {
   runApp(const App());
 }
 
-final GoRouter _router = GoRouter(
-  routes: <RouteBase>[
-    GoRoute(
-      path: '/',
-      builder: (BuildContext context, GoRouterState state) {
-        return const Login();
-      },
-      routes: <RouteBase>[
-        GoRoute(
-          path: 'details',
-          builder: (BuildContext context, GoRouterState state) {
-            return const SignUp();
-          },
-        ),
-      ],
-    ),
-  ],
-);
-
-class App extends StatefulWidget {
+class App extends StatelessWidget {
   const App({super.key});
 
   @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: _router,
+    return const GetMaterialApp(
+      home: Login(),
     );
   }
 }
