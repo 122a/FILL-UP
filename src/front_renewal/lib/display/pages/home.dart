804913549379fe55507a34a5f2787a2195463b22
chan <chan0822kr@gmail.com>
2024-05-17 10:11:38 +0900

    commit 22
     • lib > display > pages > home.dart
      - 코드 가독성 및 유지 보수를 위해서  _lead 함수와 _ends 함수를 작성한 후, 각각 leading 파라미터와 actions 파라미터에 넘김.
      그리고 주석 추가

--- src/front_renewal/lib/display/pages/home.dart
+++ src/front_renewal/lib/display/pages/home.dart
@@ -6,6 +6,7 @@ class Home extends StatelessWidget {
   const Home({super.key});
 
   Widget _lead() {
+    // 상단바 좌측에 들어갈 아이콘들을 그냥 Row로 묶어서 반환하는 메서드를 작성했음. 이거를 한번에 leading에 다 때려박으면 코드 너무 난잡해질까봐 이렇게 했음.
     return Row(
       children: [
         const SizedBox(width: 5),
@@ -26,75 +27,84 @@ class Home extends StatelessWidget {
     );
   }
 
+  List<Widget> _ends() {
+    // 코드 가독성을 위해서 상단바 우측 끝 아이콘들을 그냥 메서드로 리턴했음.
+    return [
+      // 상단바 우측 아이콘들
+      Container(
+        width: 35,
+        height: 35,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          border: Border.all(color: const Color.fromARGB(38, 158, 158, 158)),
+        ),
+        child: IconButton(
+          onPressed: () {},
+          icon: SvgPicture.asset(
+            'assets/icons/Youtube.svg',
+            width: 50,
+          ),
+        ),
+      ),
+      const SizedBox(
+        //상단바 우측 아이콘 들끼리의 간격
+        width: 15,
+      ),
+      Container(
+        //reddit 아이콘 --> 후에 구글로 수정할 것.!!!
+        width: 35,
+        height: 35,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          border: Border.all(color: const Color.fromARGB(38, 158, 158, 158)),
+        ),
+        child: IconButton(
+          onPressed: () {},
+          icon: SvgPicture.asset(
+            'assets/icons/Reddit.svg',
+            width: 50,
+          ),
+        ),
+      ),
+      const SizedBox(
+        width: 5,
+      ),
+      IconButton(
+        //검색 버튼
+        onPressed: () {},
+        icon: const Icon(
+          Icons.search,
+          size: 25,
+        ),
+      ),
+      const SizedBox(
+        width: 5,
+      )
+    ];
+  }
+
+  // 여기서 부터 빌드
   @override
   Widget build(BuildContext context) {
     return SafeArea(
+      // 동영상 창 구현을 위해서 제일 상위에 쓴 위젯
       child: CustomScrollView(
+        // 스크롤 뷰 구현을 위해서 쓰는 위젯인 듯?
         slivers: [
+          //스크롤뷰 자식요소들을 담는 named parameter 같음.
           SliverAppBar(
             //상단 바
-            leading: _lead(), //상단바 좌측 아이콘
+            leading: _lead(), //상단바 (좌측) 아이콘
             leadingWidth: 200,
             floating: true,
             snap: true,
-            actions: [
-              // 상단바 우측 아이콘들
-              Container(
-                width: 35,
-                height: 35,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  border: Border.all(
-                      color: const Color.fromARGB(38, 158, 158, 158)),
-                ),
-                child: IconButton(
-                  onPressed: () {},
-                  icon: SvgPicture.asset(
-                    'assets/icons/Youtube.svg',
-                    width: 50,
-                  ),
-                ),
-              ),
-              const SizedBox(
-                //상단바 우측 아이콘 들끼리의 간격
-                width: 15,
-              ),
-              Container(
-                //reddit 아이콘
-                width: 35,
-                height: 35,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  border: Border.all(
-                      color: const Color.fromARGB(38, 158, 158, 158)),
-                ),
-                child: IconButton(
-                  onPressed: () {},
-                  icon: SvgPicture.asset(
-                    'assets/icons/Reddit.svg',
-                    width: 50,
-                  ),
-                ),
-              ),
-              const SizedBox(
-                width: 5,
-              ),
-              IconButton(
-                //검색 버튼
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.search,
-                  size: 25,
-                ),
-              ),
-              const SizedBox(
-                width: 5,
-              )
-            ],
+            actions: _ends(), //상단바 (우측) 아이콘
           ),
+          //--------------------------- 선. 위쪽으로 AppBar, 아랫쪽으로 동영상 List
           SliverList(
             //동영상 썸네일이나 정보 같은 것들, 동영상 위젯들
             delegate: SliverChildBuilderDelegate(
+              //delegate 파라미터를 사용해서 동영상들의 공통적인 특징을 정해주는 것 같음.
               (context, index) {
                 return Padding(
                   //동영상 요소들의 특징과 사이 간격
