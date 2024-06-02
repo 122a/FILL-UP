6fdcdf36bea0b69aa84cf039b70f221f6f9c8ae9
122a <dlaehdms08@naver.com>
2024-06-02 15:52:56 +0900

    commit 30
    1. 사진 스택 문제 해결.
    2. 내 계정 관리하기를 통해 사진, 이름 변경 추가 기능
    3. 아이콘 클릭시 로그아웃 여부 및 Login 화면 이동 추가

--- src/front_renewal/lib/display/pages/my.dart
+++ src/front_renewal/lib/display/pages/my.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import 'package:front_renewal/controller/app_controller.dart';
 import 'package:front_renewal/display/pages/account_my.dart'; // 계정 관리 페이지 추가
+import 'package:front_renewal/auth/login.dart'; // 로그인 페이지 추가
 
 class My extends StatelessWidget with WidgetsBindingObserver {
   const My({super.key});
@@ -11,6 +12,7 @@ class My extends StatelessWidget with WidgetsBindingObserver {
   Widget build(BuildContext context) {
     final TimerController timerController = Get.put(TimerController());
     final AccountController accountController = Get.put(AccountController());
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -30,102 +32,177 @@ class My extends StatelessWidget with WidgetsBindingObserver {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          // 계정 정보 섹션
-          Expanded(
-            flex: 2, // 20%
-            child: Padding(
-              padding:
-                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-              child: Row( // 사용자 계정 이미지
-                children: [
-                  Obx(() {
-                    return ClipRRect(
-                      borderRadius: BorderRadius.circular(25.0),
-                      child: Image.file(
-                        File(Get.find<AccountController>()
-                            .profileImagePath
-                            .value),
-                        width: 70,
-                        height: 70,
-                        fit: BoxFit.cover,
-                        errorBuilder: (context, error, stackTrace) {
-                          return Icon(Icons.error); // 기본 아이콘으로 대체
-                        },
-                      ),
-                    );
-                  }),
-                  SizedBox(width: 8),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+      body: SingleChildScrollView(
+        // 스크롤 가능하게 설정
+        child: Column(
+          children: [
+            // 계정 정보 섹션
+            Obx(() {
+              return Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                child: Column(
+                  children: [
+                    Row(
+                      // 사용자 계정 이미지
                       children: [
-                        SizedBox(height: 35),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Obx(() {
-                              return Text(
-                                ' ${Get.find<AccountController>().userName.value}',
-                                style: TextStyle(
-                                  fontFamily: "pretendard",
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.w800,
-                                ),
-                              );
-                            }),
-                            Icon(
-                              Icons.expand_more,
-                              color: Color.fromARGB(255, 90, 90, 90),
-                            ),
-                          ],
-                        ),
-                        Text(
-                          '  chan0822kr@gmail.com',
-                          style: TextStyle(
-                            fontFamily: "SF-Pro-Rounded",
-                            fontSize: 13,
-                            fontWeight: FontWeight.w500,
-                            color: Color(0xff747474),
-                          ),
-                        ),
-                        SizedBox(height: 10),
-                        Align(
-                          alignment: Alignment.centerLeft,
-                          child: TextButton(
-                            onPressed: () {
-                              Get.to(AccountManagementPage());
-                            },
-                            style: TextButton.styleFrom(
-                              padding: EdgeInsets.symmetric(
-                                  vertical: 4.0, horizontal: 8.0),
-                              minimumSize: Size(0, 0),
-                              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                            ),
-                            child: Text(
-                              "내 계정 관리하기",
-                              style: TextStyle(
-                                fontFamily: "SF-Pro-Rounded",
-                                fontSize: 13,
-                                fontWeight: FontWeight.w500,
-                                color: Color(0xff747474),
+                        Obx(() {
+                          final imagePath =
+                              accountController.profileImagePath.value;
+                          return ClipRRect(
+                            borderRadius: BorderRadius.circular(25.0),
+                            child: imagePath == 'assets/profile.png'
+                                ? Image.asset(
+                                    imagePath,
+                                    width: 70,
+                                    height: 70,
+                                    fit: BoxFit.cover,
+                                    errorBuilder: (context, error, stackTrace) {
+                                      return Icon(Icons.error); // 기본 아이콘으로 대체
+                                    },
+                                  )
+                                : Image.file(
+                                    File(imagePath),
+                                    width: 70,
+                                    height: 70,
+                                    fit: BoxFit.cover,
+                                    errorBuilder: (context, error, stackTrace) {
+                                      return Icon(Icons.error); // 기본 아이콘으로 대체
+                                    },
+                                  ),
+                          );
+                        }),
+                        SizedBox(width: 8),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(height: 35),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Obx(() {
+                                    return Text(
+                                      ' ${accountController.userName.value}',
+                                      style: TextStyle(
+                                        fontFamily: "pretendard",
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.w800,
+                                      ),
+                                    );
+                                  }),
+                                  GestureDetector(
+                                    onTap: () {
+                                      accountController.toggleExpanded();
+                                    },
+                                    child: Icon(
+                                      accountController.isExpanded.value
+                                          ? Icons.expand_less
+                                          : Icons.expand_more,
+                                      color: Color.fromARGB(255, 90, 90, 90),
+                                    ),
+                                  ),
+                                ],
                               ),
-                            ),
+                              Text(
+                                '  chan0823kr@gmail.com',
+                                style: TextStyle(
+                                  fontFamily: "SF-Pro-Rounded",
+                                  fontSize: 13,
+                                  fontWeight: FontWeight.w500,
+                                  color: Color(0xff747474),
+                                ),
+                              ),
+                              SizedBox(height: 10),
+                              Align(
+                                alignment: Alignment.centerLeft,
+                                child: TextButton(
+                                  onPressed: () {
+                                    Get.to(AccountManagementPage());
+                                  },
+                                  style: TextButton.styleFrom(
+                                    padding: EdgeInsets.symmetric(
+                                        vertical: 4.0, horizontal: 8.0),
+                                    minimumSize: Size(0, 0),
+                                    tapTargetSize:
+                                        MaterialTapTargetSize.shrinkWrap,
+                                  ),
+                                  child: Text(
+                                    "내 계정 관리하기",
+                                    style: TextStyle(
+                                      fontFamily: "SF-Pro-Rounded",
+                                      fontSize: 13,
+                                      fontWeight: FontWeight.w500,
+                                      color: Color(0xff747474),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
                         ),
                       ],
                     ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Divider(color: Color.fromARGB(255, 224, 224, 224)),
-          // 실행 시간 섹션
-          Expanded(
-            flex: 3, // 35%
-            child: Padding(
+                    if (accountController.isExpanded.value)
+                      Padding(
+                        padding: const EdgeInsets.only(top: 0),
+                        child: Align(
+                          alignment: Alignment.centerRight,
+                          child: Padding(
+                            padding:
+                                const EdgeInsets.only(right: 30.0), // 오른쪽 여백 추가
+                            child: TextButton(
+                              onPressed: () {
+                                showDialog(
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    return AlertDialog(
+                                      title: Text("계정에서 로그아웃",
+                                          style: TextStyle(
+                                            fontFamily: "SF-Pro-Rounded",
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.w800,
+                                          )),
+                                      content: Text("정말 로그아웃 하시겠습니까?"),
+                                      actions: [
+                                        TextButton(
+                                          onPressed: () {
+                                            Get.to(Login()); // 로그인 페이지로 이동
+                                          },
+                                          child: Text("예"),
+                                        ),
+                                        TextButton(
+                                          onPressed: () {
+                                            Get.back();
+                                          },
+                                          child: Text("아니오"),
+                                        ),
+                                      ],
+                                    );
+                                  },
+                                );
+                              },
+                              child: Text(
+                                "계정에서 로그아웃",
+                                style: TextStyle(
+                                  fontFamily: "SF-Pro-Rounded",
+                                  fontSize: 13,
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.red,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                  ],
+                ),
+              );
+            }),
+            Divider(color: Color.fromARGB(255, 224, 224, 224)),
+            // 실행 시간 섹션
+            Padding(
               padding: const EdgeInsets.all(16.0),
               child: Row(
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -153,6 +230,7 @@ class My extends StatelessWidget with WidgetsBindingObserver {
                           final elapsedTime = timerController.elapsedTime.value;
                           final minutes = (elapsedTime.inMinutes % 60); // 분
                           final seconds = (elapsedTime.inSeconds % 60); // 초
+                          final isDoubleDigit = minutes >= 10 && seconds >= 10;
                           return Padding(
                             padding: const EdgeInsets.symmetric(vertical: 8.0),
                             child: Text(
@@ -163,6 +241,9 @@ class My extends StatelessWidget with WidgetsBindingObserver {
                                 fontSize: 50,
                                 fontWeight: FontWeight.w900,
                                 color: Color.fromARGB(255, 9, 150, 245),
+                                letterSpacing: isDoubleDigit
+                                    ? -2.0
+                                    : 0.0, // 두 자릿수일 때 간격 줄이기
                               ),
                             ),
                           );
@@ -173,12 +254,9 @@ class My extends StatelessWidget with WidgetsBindingObserver {
                 ],
               ),
             ),
-          ),
-          Divider(color: Color.fromARGB(255, 224, 224, 224)),
-          // 타임 슬롯 섹션
-          Expanded(
-            flex: 4, // 45%
-            child: Padding(
+            Divider(color: Color.fromARGB(255, 224, 224, 224)),
+            // 타임 슬롯 섹션
+            Padding(
               padding: const EdgeInsets.all(16.0),
               child: Row(
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -214,8 +292,8 @@ class My extends StatelessWidget with WidgetsBindingObserver {
                 ],
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
