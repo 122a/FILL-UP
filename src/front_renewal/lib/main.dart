a02c43337fa4d88a87d00deacf25c4db8e967249
122a <dlaehdms08@naver.com>
2024-06-08 01:13:11 +0900

    commit 31
    1. my.dart 충돌 문제 해결
    2. 임시 타임 스택 구현
    3. 로그인 및 회원가입 구현
    
    + signUp.dart에서 await 오류 (이유 찾는 중.)

--- src/front_renewal/lib/main.dart
+++ src/front_renewal/lib/main.dart
@@ -6,8 +6,8 @@ import 'package:front_renewal/auth/login.dart';
 import 'package:front_renewal/auth/signup.dart';
 import 'package:front_renewal/binding/init_binding.dart';
 import 'package:front_renewal/controller/app_controller.dart';
-import 'package:front_renewal/controller/detail_controller.dart';
-import 'package:front_renewal/controller/home_controller.dart';
+// import 'package:front_renewal/controller/detail_controller.dart';
+// import 'package:front_renewal/controller/home_controller.dart';
 import 'package:front_renewal/display/app.dart';
 import 'package:front_renewal/display/pages/datail.dart';
 import 'package:front_renewal/display/pages/search.dart';
@@ -16,7 +16,14 @@ import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+      options: FirebaseOptions(
+    apiKey: 'key',
+    appId: 'id',
+    messagingSenderId: 'sendid',
+    projectId: 'myapp',
+    storageBucket: 'myapp-b9yt18.appspot.com',
+  ));
   Get.put(TimerController()); // 타이머 컨트롤러 초기화
   runApp(const FillUp());
 }
